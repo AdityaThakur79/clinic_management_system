7d1827deff23f8c1699db8e8fba5011a81f3ac1b
Aditya Thakur <adityathakurbusiness@gmail.com>
2025-09-23 11:59:04 +0530

    Build Pushed

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:3000",
       "http://127.0.0.1:8080",
+      "https://aartiket-speech-and-hearing-care.onrender.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
