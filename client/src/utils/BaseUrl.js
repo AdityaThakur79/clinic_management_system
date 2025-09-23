29eff00ffd98ea9d4bb942f55be7bd591e819a8f
Aditya Thakur <adityathakurbusiness@gmail.com>
2025-09-23 11:55:54 +0530

    Base URL Changed

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api";
+// export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://aartiket-speech-and-hearing-care.onrender.com/api";
