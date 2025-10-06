30ca93db1e83f4bb226910cd2d24191b00776895
Aditya Thakur <adityathakurbusiness@gmail.com>
2025-10-06 13:28:06 +0530

    MF

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8080/api";
-// export const BASE_URL = "https://aartiket-speech-and-hearing-care.onrender.com/api";
+export const BASE_URL = "https://aartiket-speech-and-hearing-care.onrender.com/api";
 
 //aditya
-export const BASE_URL = "https://clinic-management-system-pz88.onrender.com/api";
+// export const BASE_URL = "https://clinic-management-system-pz88.onrender.com/api";
